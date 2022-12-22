82ff49d4becec8d8240107326ef21941c895cf21
Isaac Kim <kisaac95@yahoo.com>
2022-12-21 22:42:33 -0800

    erething

--- src/App.js
+++ src/App.js
@@ -1,38 +1,63 @@
-import { useEffect, useState } from 'react'
+import { useState, useRef } from 'react'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import Gallery from './components/Gallery'
 import SearchBar from './components/SearchBar'
+import { DataContext } from './context/DataContext'
+import { SearchContext } from './context/SearchContext'
 import AlbumView from './components/AlbumView'
 import ArtistView from './components/ArtistView'
+import { Fragment } from 'react'
 
-function App(){
-    let [search, setSearch] = useState('')
+
+function App() {
     let [message, setMessage] = useState('Search for Music!')
-    let [data, setData] = useState([])
+ let [data, setData] = useState([])
+ let searchInput = useRef('')
 
-    useEffect(() => {
+    const API_URL = 'https://itunes.apple.com/search?term='
+    
+    const handleSearch = (e, term) => {
+        e.preventDefault()
+        // Fetch Data
         const fetchData = async () => {
-            document.title = `${search} Music`
-            const response = await fetch('https://itunes.apple.com/search?term=the%20grateful%20dead')
+            document.title = `${term} Music`
+            const response = await fetch(API_URL + term)
             const resData = await response.json()
             if (resData.results.length > 0) {
-              setData(resData.results)
+                // Set State and Context value
+                return setData(resData.results)
             } else {
-              setMessage('Not Found')
+                return setMessage('Not Found')
             }
         }
         fetchData()
-    }, [search])
-
+    }
+    
     return (
-        <div>
+        <div className="App">
+          {message}
+          <Router>
+            <Routes>
+              <Route path="/" element={
+                <Fragment>
+                  <SearchContext.Provider value={{
+                  term: searchInput,
+                  handleSearch: handleSearch
+                  }}>
             <SearchBar />
-            {message}
-            <Gallery />
+                  </SearchContext.Provider>
+            <DataContext.Provider value={data}>
+              <Gallery />
+            </DataContext.Provider>
+                </Fragment>
+              } />
+              <Route path ='/album/:id' element={<AlbumView />} />
+              <Route path ='/artist/:id' element={<ArtistView />} />
+            </Routes>
+          </Router>
         </div>
-    )
+    );
 }
 
-export default App
-
+export default App;
 
